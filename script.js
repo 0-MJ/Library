a6051f9acc1433a2d6c1c25bd5e4f1cb2b67541a
Mervyn <102972493+0M-J@users.noreply.github.com>
2023-02-01 03:38:17 +0530

    added css n wrote a  rough pseudo code for js

---
+++ script.js
@@ -0,0 +1,19 @@
+//Simple Array to store all the book objects//
+let myLibrary = [];
+//Book Constructor which creates book objects//
+function Book() {
+    // the constructor...
+  }
+//Function to add book to the library//
+function addBookToLibrary() {
+    
+  }
+
+//A function to Loop through the myLibrary array and displays each book on the page.//
+
+//New Book button that brings up a form for users to input the details for the newbook//
+
+
+//A button to remove the book from the library on each book.//
+
+//A button to change  read status on each book's Display//
