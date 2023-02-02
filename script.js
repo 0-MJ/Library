33115662ed31ad8d55d94b205017736ddd006503
Mervyn <102972493+0M-J@users.noreply.github.com>
2023-02-02 15:01:11 +0530

    update script
    - added a proper constructor for creating books.
    - added few test books
    - a function to push book to library array
    - created a for each to loop through the array.

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 //Simple Array to store all the book objects//
 let myLibrary = [];
 //Book Constructor which creates book objects//
-function Book() {
-    this.title = " ";
-    this.author = " ";
-    this.totalPages = " ";
+function Book(title,author,pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
   }
-//Function to add book to the library//
-function addBookToLibrary() {
-    
-  }
+//Test book//
+let book1 = new Book('Animal Farm','George Orwell',
+ 104);
+ addBookToLibrary(book1);
+let book2 = new Book('Brave New World','Aldous Huxley',184);
+addBookToLibrary(book2);
 
+
+//Function to add book to the library//
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+  
+  }
 //A function to Loop through the myLibrary array and displays each book on the page.//
+myLibrary.forEach(function(books){
+  console.log(books);
+})
 
 //New Book button that brings up a form for users to input the details for the newbook//
 
